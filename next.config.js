a89e981e8a165b05bd31595f90dd747a7d5700de
mattduff36 <mattduff36@gmail.com>
2025-05-01 23:13:54 +0100

    Update Next.js configuration for proper deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    domains: ['leebarrow.vercel.app'],
   },
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
